6aa16baf9993bbf7cbe8f7d1b3f27e06e88c9d5c
zb-huangjinpeng <zb-huangjinpeng@kingmed.com.cn>
2023-10-20 15:11:44 +0800

    feat: :sparkles: 添加容器组件BaseCard

--- src/common/plugins/components.js
+++ src/common/plugins/components.js
@@ -1,10 +1,12 @@
 import { AppPage } from '@/components/page'
 import { BaseIcon } from '@/components/icon'
+import { BaseCard } from '@/components/card'
 
 const registerComponents = {
   install: (Vue) => {
     Vue.component('AppPage', AppPage)
     Vue.component('BaseIcon', BaseIcon)
+    Vue.component('BaseCard', BaseCard)
   }
 }
 
---
+++ src/components/card/BaseCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="base-card">
+    <div v-if="title || $slots.title || $slots.extra" class="base-card__header">
+      <div class="base-card__left">
+        <slot name="title">
+          <div v-if="title" class="base-card__title">{{ title }}</div>
+        </slot>
+      </div>
+      <div class="base-card__right">
+        <slot name="extra" />
+      </div>
+    </div>
+    <div class="base-card__body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="less">
+.base-card {
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 4px #edf0f4;
+  & + & {
+    margin-top: 16px;
+  }
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 16px;
+  }
+  &__title {
+    display: flex;
+    align-items: center;
+    color: @primary-color;
+    &::before {
+      display: inline-block;
+      width: 4px;
+      height: 14px;
+      margin-right: 8px;
+      content: '';
+      background-color: @primary-color;
+      border-radius: 6px;
+    }
+  }
+}
+</style>

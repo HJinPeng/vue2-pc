623d9188f1d18d4dd2a5bdacdcaa62b0225e2896
jinpengh <742463015@qq.com>
2023-11-15 22:57:39 +0800

    style: :lipstick: 备注调整

--- src/api/system/dict/item.js
+++ src/api/system/dict/item.js
@@ -24,6 +24,7 @@ export function getDictItemPageApi(params) {
  *
  * @export
  * @param {Object} data
+ * @param {String} params.dictId 字典id
  * @param {String} data.dictItemCode
  * @param {String} data.dictItemName
  * @param {String} params.sort 排序
@@ -44,8 +45,9 @@ export function addDictItemApi(data) {
  *
  * @export
  * @param {Object} data
- * @param {String} data.realCode
- * @param {String} data.realname
+ * @param {String} params.dictId 字典id
+ * @param {String} data.dictItemCode
+ * @param {String} data.dictItemName
  * @param {String} params.sort 排序
  * @param {String} params.status 状态
  * @returns {*}
